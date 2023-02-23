fb0d97744a18bbd4484aeefe41f74f28623e8ff4
HSU JUI SHENG <zed2786328@gmail.com>
2023-02-23 13:29:44 +0800

    adjust trade config BTCUSDT

--- src/trade-config.js
+++ src/trade-config.js
@@ -1,8 +1,8 @@
 const tradeConfig = {
-  QUOTE_CURRENCY: "BUSD",
-  SYMBOL: "BTCBUSD",
-  LEVERAGE: 50, // 50x
-  FEE_RATE: 3 / 100 / 100, // 0.03%
+  QUOTE_CURRENCY: "USDT",
+  SYMBOL: "BTCUSDT",
+  LEVERAGE: 125, // 125x
+  FEE_RATE: 4 / 100 / 100, // 0.04%
   TP_SL_RATE: 10 / 100, // 10%
   INITIAL_QUANTITY: 1 / 1000 // 0.001
 };
