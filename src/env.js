d7daf0754e16df496420a6ecc785f36fa0b49964
HSU JUI SHENG <zed2786328@gmail.com>
2023-03-03 15:22:41 +0800

    optimize configs to json

--- src/env.js
+++ src/env.js
@@ -1,8 +1,9 @@
 import { readFile } from "node:fs/promises";
 
-const filePath =
-  process.env.NODE_ENV === "production" ? "env-prod.json" : "env-dev.json";
-const contents = await readFile(filePath);
+const path =
+  process.env.NODE_ENV === "production" ? "./env-prod.json" : "./env-dev.json";
+const filePath = new URL(path, import.meta.url);
+const contents = await readFile(filePath, { encoding: "utf8" });
 const env = JSON.parse(contents);
 
 export default env;
--- src/trade-config.js
+++ src/trade-config.js
@@ -1,11 +1,7 @@
-const tradeConfig = {
-  BASE_ASSET: "BTC",
-  QUOTE_ASSET: "USDT",
-  SYMBOL: "BTCUSDT",
-  LEVERAGE: 125, // 125x
-  FEE_RATE: 4 / 100 / 100, // 0.04%
-  TP_SL_RATE: 10 / 100, // 10%
-  INITIAL_QUANTITY: 1 / 1000 // 0.001
-};
+import { readFile } from "node:fs/promises";
+
+const filePath = new URL("./trade-config.json", import.meta.url);
+const contents = await readFile(filePath, { encoding: "utf8" });
+const tradeConfig = JSON.parse(contents);
 
 export default tradeConfig;
